f2c3d62443f631b881dee7db34ffba4f3885a20e
LisaLowagie <lowagielisa@gmail.com>
2024-11-29 14:47:02 +0100

    Changed background image

--- src/assets/home.jsx
+++ src/assets/home.jsx
@@ -1,69 +1,61 @@
 import { Link } from "react-router-dom";
 import Pricing from "../components/pricing";
 import WhyUs from "../components/whyUs";
-import WorkflowCarousel from "../components/workflowCarousel";
+import test1 from "../../public/media/img/1.png";
 
 const Home = () => {
-    return (
-        <>
-            <div
-                className="mx-auto
+	return (
+		<>
+			<div
+				className="mx-auto
             "
-            >
-                <div className="intro py-80 flex flex-col content-center">
-                    <h2
-                        className="intro text-center"
-                        style={{ background: "none" }}
-                    >
-                        At Bel'Azur Coding, we specialize in creating tailored,
-                        high-performance websites that are as unique as your
-                        business. We offer a strong commitment to quality and a
-                        flair for elegant, hand-coded solutions.
-                    </h2>
-                    <h2
-                        className="intro text-center mt-4"
-                        style={{ background: "none" }}
-                    >
-                        Together, we turn your digital dreams into reality.
-                    </h2>
-                </div>
-                <div>
-                    <WhyUs />
-                </div>
-                <div>
-                    <Pricing />
-                </div>
-                <div className="philosophy px-8 py-12">
-                    <h1 className="text-center">OUR PHILOSOPHY</h1>
-                    <p className="text-center">
-                        We believe in clean code, clean design, and clear
-                        communication. Every project we take on is a
-                        collaborative journey, and your satisfaction is our
-                        ultimate goal.
-                    </p>
-                    <p className="text-center">
-                        From concept to launch, we're here to ensure your
-                        digital presence makes a powerful impact.
-                    </p>
-                </div>
-                {/* <Workflow /> */}
-                <WorkflowCarousel />
-                <div className="text-center">
-                    <h2>
-                        Let's transform your ideas into an exceptional online
-                        experience.
-                    </h2>
-                    <h2 className="mb-4">
-                        Place your order for a one-pager today or contact us to
-                        discuss a custom project.
-                    </h2>
-                    <Link to="/contact">
-                        <button>Let's get in touch</button>
-                    </Link>
-                </div>
-            </div>
-        </>
-    );
+			>
+				<div className="intro py-80 flex flex-col content-center">
+					<h2 className="text-center">
+						At Bel'Azur Coding, we specialize in creating tailored,
+						high-performance websites that are as unique as your
+						business. We offer a strong commitment to quality and a
+						flair for elegant, hand-coded solutions.
+					</h2>
+					<h2 className=" text-center mt-4">
+						Together, we turn your digital dreams into reality.
+					</h2>
+				</div>
+				<div>
+					<WhyUs />
+				</div>
+				<div>
+					<Pricing />
+				</div>
+				<div className="philosophy px-8 py-12">
+					<h1 className="text-center">OUR PHILOSOPHY</h1>
+					<p className="text-center">
+						We believe in clean code, clean design, and clear
+						communication. Every project we take on is a
+						collaborative journey, and your satisfaction is our
+						ultimate goal.
+					</p>
+					<p className="text-center">
+						From concept to launch, we're here to ensure your
+						digital presence makes a powerful impact.
+					</p>
+				</div>
+				<div className="text-center">
+					<h2>
+						Let's transform your ideas into an exceptional online
+						experience.
+					</h2>
+					<h2 className="mb-4">
+						Place your order for a one-pager today or contact us to
+						discuss a custom project.
+					</h2>
+					<Link to="/contact">
+						<button>Let's get in touch</button>
+					</Link>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Home;
--- src/structure/navbar.jsx
+++ src/structure/navbar.jsx
@@ -9,106 +9,103 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
 
 const navigation = [
-    { name: "HOME", href: "/" },
-    { name: "SERVICES", href: "/services" },
-    { name: "ABOUT", href: "/about" },
-    { name: "CONTACT", href: "/contact" },
+	{ name: "HOME", href: "/" },
+	{ name: "SERVICES", href: "/services" },
+	{ name: "ABOUT", href: "/about" },
+	{ name: "CONTACT", href: "/contact" },
 ];
 
 const Navbar = () => {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    return (
-        <header
-            className="absolute inset-x-0 top-0 z-100 transition-all duration-300 "
-            style={{ backgroundColor: "#f2bb05" }}
-        >
-            <nav
-                aria-label="Global"
-                className="flex justify-between items-center px-8 pt-2"
-            >
-                {/* Logo */}
-                <div className="flex items-center">
-                    <a href="/" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Bel'Azur Coding</span>
-                        <img src={Azur} alt="logo" className="h-20 w-auto" />
-                    </a>
-                </div>
+	return (
+		<header className="absolute inset-x-0 top-0 z-100 transition-all duration-300 ">
+			<nav
+				aria-label="Global"
+				className="flex justify-between items-center px-8 pt-2"
+			>
+				{/* Logo */}
+				<div className="flex items-center">
+					<a href="/" className="-m-1.5 p-1.5">
+						<span className="sr-only">Bel'Azur Coding</span>
+						<img src={Azur} alt="logo" className="h-20 w-auto" />
+					</a>
+				</div>
 
-                {/* Menu */}
-                <div className="flex">
-                    <button
-                        type="button"
-                        onClick={() => setMobileMenuOpen(true)}
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-black"
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <img
-                            src={MENU}
-                            style={{ width: "50px", height: "50px" }}
-                        />
-                    </button>
-                </div>
-            </nav>
-            <Dialog
-                open={mobileMenuOpen}
-                onClose={setMobileMenuOpen}
-                className=""
-            >
-                <div className="fixed inset-0 z-50" />
-                <DialogPanel
-                    className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-                    style={{ backgroundColor: "#f2bb05" }}
-                >
-                    <div className="flex items-center justify-between">
-                        <button
-                            type="button"
-                            onClick={() => setMobileMenuOpen(false)}
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                        >
-                            <span className="sr-only">Close menu</span>
-                            <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-                        </button>
-                    </div>
-                    <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10">
-                            <div className="space-y-2 py-6">
-                                {navigation.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:text-white"
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </div>
-                            <div className="flex justify-evenly pt-4">
-                                <button>
-                                    <Link
-                                        to="https://www.facebook.com"
-                                        target="_blank"
-                                    >
-                                        <FontAwesomeIcon icon={faFacebook} />{" "}
-                                        Facebook
-                                    </Link>
-                                </button>
-                                <button>
-                                    <Link
-                                        to="https://www.instagram.com"
-                                        target="_blank"
-                                    >
-                                        <FontAwesomeIcon icon={faInstagram} />{" "}
-                                        Instagram
-                                    </Link>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </DialogPanel>
-            </Dialog>
-        </header>
-    );
+				{/* Menu */}
+				<div className="flex">
+					<button
+						type="button"
+						onClick={() => setMobileMenuOpen(true)}
+						className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-black"
+					>
+						<span className="sr-only">Open main menu</span>
+						<img
+							src={MENU}
+							style={{ width: "50px", height: "50px" }}
+						/>
+					</button>
+				</div>
+			</nav>
+			<Dialog
+				open={mobileMenuOpen}
+				onClose={setMobileMenuOpen}
+				className=""
+			>
+				<div className="fixed inset-0 z-50" />
+				<DialogPanel
+					className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+					style={{ backgroundColor: "#f2bb05" }}
+				>
+					<div className="flex items-center justify-between">
+						<button
+							type="button"
+							onClick={() => setMobileMenuOpen(false)}
+							className="-m-2.5 rounded-md p-2.5 text-gray-700"
+						>
+							<span className="sr-only">Close menu</span>
+							<XMarkIcon aria-hidden="true" className="h-6 w-6" />
+						</button>
+					</div>
+					<div className="mt-6 flow-root">
+						<div className="-my-6 divide-y divide-gray-500/10">
+							<div className="space-y-2 py-6">
+								{navigation.map((item) => (
+									<a
+										key={item.name}
+										href={item.href}
+										className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:text-white"
+									>
+										{item.name}
+									</a>
+								))}
+							</div>
+							<div className="flex justify-evenly pt-4">
+								<button>
+									<Link
+										to="https://www.facebook.com"
+										target="_blank"
+									>
+										<FontAwesomeIcon icon={faFacebook} />{" "}
+										Facebook
+									</Link>
+								</button>
+								<button>
+									<Link
+										to="https://www.instagram.com"
+										target="_blank"
+									>
+										<FontAwesomeIcon icon={faInstagram} />{" "}
+										Instagram
+									</Link>
+								</button>
+							</div>
+						</div>
+					</div>
+				</DialogPanel>
+			</Dialog>
+		</header>
+	);
 };
 
 export default Navbar;
