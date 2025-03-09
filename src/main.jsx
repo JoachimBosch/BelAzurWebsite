a91e90027b5fe43d500107ed71e149b19f9b4211
Joachim Bosch <bosch.joachim@outlook.com>
2025-03-09 14:41:26 +0100

    build

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Footer from "./structure/footer";
 import CookieModal from "./components/cookieModal";
 
 createRoot(document.getElementById("root")).render(
-    <Router>
+    <Router basename="/BelAzurWebsite">
         <MyProvider>
             <Navbar />
             <AuthRoutes />
